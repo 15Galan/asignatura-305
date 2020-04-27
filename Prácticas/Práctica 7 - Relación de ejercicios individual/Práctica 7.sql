f5e359a860415337f7cbfe6c38aad617982e2ac9
Galán <antoniogalanG15@gmail.com>
2020-04-27 21:26:28 +0200

    Entrega de la Práctica 7 añadida
    Ejercicios resueltos excepto el 9 y el 10.

--- Prácticas/Práctica 7 - Relación de ejercicios individual/Práctica 7.sql
+++ Prácticas/Práctica 7 - Relación de ejercicios individual/Práctica 7.sql
@@ -106,10 +106,10 @@ CREATE OR REPLACE
 
 
 -- Ejercicio 7
-DESC all_objects;
-
-SELECT *
-    FROM all_objects;
+    DESC all_objects;
+    
+    SELECT *
+        FROM all_objects;
 
 CREATE TABLE tb_objetos (
     nombre VARCHAR2(128),
@@ -120,10 +120,10 @@ CREATE TABLE tb_objetos (
     esquema_original VARCHAR2(128));
 
 CREATE OR REPLACE
-    PROCEDURE rellenar (n NUMBER) AS
-        BEGIN
-            DECLARE
-                CURSOR filas IS (
+    PROCEDURE rellenar_objetos AS   -- Solo se usa en este apartado, en el siguiente
+        BEGIN                       -- ejercicio deja de funcionar (se añaden columnas)
+            DECLARE                
+                CURSOR filas IS
                     SELECT
                         object_name,
                         object_id,
@@ -131,16 +131,73 @@ CREATE OR REPLACE
                         last_ddl_time,
                         object_type,
                         owner
-                            FROM all_objects)
-                        FOR UPDATE;
+                            FROM all_objects;
+                        -- FOR UPDATE;
                         
             BEGIN            
                 FOR fila IN filas LOOP
-                    NULL;
+                    INSERT INTO tb_objetos
+                        VALUES (
+                            fila.object_name,
+                            fila.object_id,
+                            fila.created,
+                            fila.last_ddl_time,
+                            fila.object_type,
+                            fila.owner);
+                            
                 END LOOP;
                 
                 COMMIT;
             END;
             
-        END rellenar;
+        END rellenar_objetos;
 /
+
+EXEC rellenar_objetos;
+
+
+-- Ejercicio 8
+CREATE TABLE tb_estilos (
+    tipo_objeto VARCHAR2(23),
+    prefijo VARCHAR2(10));
+
+INSERT INTO tb_estilos
+    VALUES ('procedure', 'pr_');
+
+
+-- Ejercicio 9
+ALTER TABLE tb_objetos
+    ADD estado VARCHAR2(10)
+    ADD nombre_correcto VARCHAR2(178);
+
+CREATE OR REPLACE
+    PROCEDURE pr_comprobar(esquema IN VARCHAR2) AS
+        BEGIN
+            DECLARE
+                                        
+            BEGIN
+                
+                NULL;
+                
+            END;
+            
+        END pr_comprobar;
+/
+
+
+-- Ejercicio 10
+CREATE TABLE tb_errores (
+    fecha DATE,
+    rutina VARCHAR2(128),
+    codigo NUMBER,
+    mensaje VARCHAR2(128));
+
+    CREATE PROCEDURE pr_select_mas_una_fila AS
+        var_fila all_tables%ROWTYPE;
+        
+        BEGIN
+            SELECT * INTO var_fila
+                FROM all_tables;
+        
+        END pr_select_mas_una_fila;
+    /
